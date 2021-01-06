ff0b57b8eeb45914b222b4e92bc81ae6f6ac10fe
rahi20 <largorahima@gmail.com>
2021-01-06 15:00:45 +0100

    deleting all the files

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.elearning.beans"},{"l":"com.elearning.common"},{"l":"com.elearning.database"},{"l":"com.elearning.db"},{"l":"com.elearning.filters"},{"l":"com.elearning.posts"},{"l":"com.elearning.servlets"}]
